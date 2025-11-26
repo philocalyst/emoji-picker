d50e08eeaa325757fb2cd5cb233a1227a9a80f87
Myles Wirth <regime_skylark.0y@icloud.com>
2025-11-26 12:18:35 -0500

    Added a get_bar_icons function

--- src/utils.rs
+++ src/utils.rs
@@ -1,4 +1,4 @@
-use std::rc::Rc;
+use std::{path::PathBuf, rc::Rc};
 
 use emoji::Emoji;
 use gpui::{Pixels, Size, size};
@@ -10,6 +10,28 @@ use crate::SEARCHER;
 pub(crate) fn calculate_emojis_per_row(container_width: f64, emoji_size: Pixels) -> usize {
 	let emojis_per_row = (container_width / emoji_size.to_f64()).floor() as usize;
 	emojis_per_row.max(1)
+}
+
+/// Returns an array of paths pointing to SVG's representing the various emoji
+/// categories
+pub(crate) fn get_bar_icons() -> Vec<PathBuf> {
+	// Rightmost words from your list
+	const DESIRED_CATAGORIES: [&str; 10] = [
+		"activity",         // activities
+		"rat",              // animals_and_nature
+		"component",        // component
+		"flag",             // flags
+		"utensils-crossed", // food_and_drink
+		"target",           // objects
+		"person-standing",  // people_and_body
+		"smile-plus",       // smileys_and_emotion
+		"shell",            // symbols
+		"ship",             // travel_and_places
+	];
+
+	let base = PathBuf::from("./lucide/icons");
+
+	DESIRED_CATAGORIES.iter().map(|name| base.join(format!("{name}.svg"))).collect()
 }
 
 /// Generates row sizes for the virtual list based on emoji count and layout
