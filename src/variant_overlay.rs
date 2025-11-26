4f6d56f4fc623553b42bd73876be78536d6c9b94
Myles Wirth <regime_skylark.0y@icloud.com>
2025-11-25 20:27:12 -0500

    Added support for reognziing when there are not real variatns

--- src/variant_overlay.rs
+++ src/variant_overlay.rs
@@ -9,28 +9,28 @@ use crate::utils::generate_skin_tone_variants;
 
 /// Renders the overlay showing skin tone variants for a selected emoji
 pub(crate) fn render(emoji: &Emoji) -> impl IntoElement {
-    div()
-        .absolute()
-        .top_0()
-        .left_0()
-        .w_full()
-        .h_full()
-        .flex()
-        .items_center()
-        .justify_center()
-        .child(
-            div()
-                .p_4()
-                .rounded_md()
-                .shadow_lg()
-                .flex()
-                .flex_row()
-                .bg(white())
-                .gap_2()
-                .children(
-                    generate_skin_tone_variants(emoji.glyph)
-                        .into_iter()
-                        .map(|variant| div().child(variant)),
-                ),
-        )
+    if let Some(variants) = generate_skin_tone_variants(emoji.glyph) {
+        div()
+            .absolute()
+            .top_0()
+            .left_0()
+            .w_full()
+            .h_full()
+            .flex()
+            .items_center()
+            .justify_center()
+            .child(
+                div()
+                    .p_4()
+                    .rounded_md()
+                    .shadow_lg()
+                    .flex()
+                    .flex_row()
+                    .bg(white())
+                    .gap_2()
+                    .children(variants.into_iter().map(|variant| div().child(variant))),
+            )
+    } else {
+        div()
+    }
 }
