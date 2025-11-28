305eda5e6dd481d97f0d8aad530195114f98ff39
Myles Wirth <regime_skylark.0y@icloud.com>
2025-11-28 18:46:29 -0500

    Change of heart

--- src/emojir.rs
+++ src/emojir.rs
@@ -1,6 +1,6 @@
-use std::{ops::Range, rc::Rc, sync::Mutex};
+use std::{ops::Range, rc::Rc};
 
-use emoji::{Emoji, EmojiEntry, Group};
+use emoji::{Emoji, EmojiEntry};
 use gpui::{Div, Entity, InteractiveElement, IntoElement, ParentElement, Pixels, Size, StatefulInteractiveElement, Styled, div, prelude::FluentBuilder, rems};
 use gpui_component::{VirtualListScrollHandle, v_virtual_list};
 
@@ -9,16 +9,16 @@ use crate::{picker::Picker, variant_overlay};
 impl Picker {
 	/// Renders a single emoji button
 	pub(crate) fn render_button(
+		emoji_idx: usize,
 		emoji: &EmojiEntry,
-		selected_emoji: Option<EmojiEntry>,
+		selected_emoji: Option<usize>,
 		entity: Entity<Picker>,
 	) -> impl IntoElement {
-		let emoji_name = emoji.emoji().name;
 		div()
-			.id(emoji_name)
+			.id(emoji_idx)
 			.relative()
 			.child(emoji.emoji().glyph)
-			.when(selected_emoji == Some(*emoji), |parent| {
+			.when(selected_emoji == Some(emoji_idx), |parent| {
 				parent.child(div().absolute().top_0().left_0().child(variant_overlay::render(emoji)))
 			})
 			.cursor_pointer()
@@ -28,7 +28,7 @@ impl Picker {
 				move |_event, _, ctx| {
 					// Update using the correct GPUI API
 					entity.update(ctx, |picker, cx| {
-						picker.selected_emoji = Some(emoji);
+						picker.selected_emoji = Some(emoji_idx);
 						cx.notify();
 					});
 					println!("{emoji:?}");
@@ -36,15 +36,18 @@ impl Picker {
 			})
 	}
 
-	/// Renders a section of emojis
-	pub(crate) fn render_section(
-		emojis_in_use: &[&EmojiEntry],
-		selected_emoji: Option<EmojiEntry>,
+	/// Renders a row of emojis for the virtual list
+	pub(crate) fn render_row(
+		start_idx: usize,
+		end_idx: usize,
+		emojis: &[&EmojiEntry],
+		selected_emoji: Option<usize>,
 		entity: Entity<Picker>,
 	) -> Div {
-		div().flex().flex_row().children(
-			emojis_in_use.iter().map(|emoji| Self::render_button(emoji, selected_emoji, entity.clone())),
-		)
+		div().flex().flex_row().children((start_idx..end_idx).map(|emoji_idx| {
+			let emoji = emojis[emoji_idx];
+			Self::render_button(emoji_idx, emoji, selected_emoji, entity.clone())
+		}))
 	}
 
 	/// Renders the emoji grid with virtual scrolling
@@ -52,7 +55,7 @@ impl Picker {
 		picker: Entity<Picker>,
 		emojis: Vec<&'static EmojiEntry>,
 		emojis_per_row: usize,
-		selected_emoji: Option<EmojiEntry>,
+		selected_emoji: Option<usize>,
 		row_sizes: Rc<Vec<Size<Pixels>>>,
 		emoji_text_size: f32,
 		scroll_handle: &VirtualListScrollHandle,
@@ -60,19 +63,18 @@ impl Picker {
 		// This list is not a long list of emojis, it's a long list of rows of emojis
 		v_virtual_list(
 			picker.clone(),
-			"emojis", // Accessible name
+			"emojis",
 			row_sizes,
-			move |_picker, active_sections: Range<usize>, _window, _ctx| {
-				active_sections
-					.map(|section_index| {
-						// Get the active group
-						let current_group: Group = Group::ALL[section_index];
+			move |_picker, active_rows: Range<usize>, _window, _ctx| {
+				active_rows
+					.map(|row_idx| {
+						// Jump to the start of where the next row should be drawn
+						let start_idx = row_idx * emojis_per_row;
 
-						// Only get those that we need
-						let relevant_emojis: Vec<&EmojiEntry> =
-							emojis.iter().filter(|emoji| emoji.emoji().group == current_group).cloned().collect();
+						// The end of this specific row
+						let end_idx = (start_idx + emojis_per_row).min(emojis.len());
 
-						Self::render_section(&relevant_emojis, selected_emoji, picker.clone())
+						Self::render_row(start_idx, end_idx, &emojis, selected_emoji, picker.clone())
 					})
 					.collect()
 			},
