6a762b77d0d42b464391f9dbf6f1cb2941344efc
Myles Wirth <regime_skylark.0y@icloud.com>
2025-12-26 21:14:35 -0500

    Row style fixes

--- src/core_row.rs
+++ src/core_row.rs
@@ -1,5 +1,6 @@
 use emoji::EmojiEntry;
-use gpui::{App, InteractiveElement, IntoElement, ParentElement, RenderOnce, StatefulInteractiveElement, Styled, Window, div};
+use gpui::{App, InteractiveElement, IntoElement, ParentElement, RenderOnce, StatefulInteractiveElement, Styled, Window, div, px};
+use gpui_component::StyledExt;
 pub(crate) use gpui_component::{ActiveTheme, Selectable, h_flex};
 
 #[derive(IntoElement)]
@@ -25,13 +26,14 @@ impl RenderOnce for EmojiRow {
 		h_flex().gap_2().children(self.emojis.iter().map(|emoji| {
 			div()
 				.id(emoji.emoji().glyph) // ID is required for jump points
-				.hover(|div| div.bg(cx.theme().accent.opacity(1.0))) // Bring out the background for hover contrast
+				.hover(|div| div.bg(cx.theme().accent.opacity(0.7))) // Bring out the background for hover contrast
 				.on_click(|_click_event, _window, _app| {
 					espanso_inject::get_injector(espanso_inject::InjectorCreationOptions::default())
 						.unwrap()
 						.send_string("hi", espanso_inject::InjectionOptions::default())
 						.expect("Shouldn't fail, I trust Espanso");
 				})
+				.corner_radii(gpui::Corners::all(px(5f32)))
 				.cursor_pointer()
 				.child(emoji.emoji().glyph)
 		}))
