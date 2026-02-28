863dc55c478d81c24138f52ef146983e6118e379
Myles Wirth <regime_skylark.0y@icloud.com>
2026-02-28 16:03:37 -0500

    Elsecase fr irrelevant here

--- src/variant_overlay.rs
+++ src/variant_overlay.rs
@@ -1,31 +1,32 @@
 use emoji::Emoji;
-use gpui::{App, InteractiveElement, IntoElement, ParentElement, Styled, div, hsla, prelude::*};
+use gpui::{
+	App, InteractiveElement, IntoElement, ParentElement, Styled, blue, div, hsla, prelude::*,
+	transparent_black,
+};
 use gpui_component::{ActiveTheme, h_flex};
 
 use crate::insert_emoji;
 
 /// Renders the list of skin tone variants for a selected emoji
 pub(crate) fn element(emoji: &Emoji, cx: &mut App) -> impl IntoElement {
-	if let Some(variants) = emoji.skin_tones {
-		h_flex()
-			.gap_2()
-			.p_2()
-			.bg(cx.theme().background)
-			.children(variants.into_iter().map(|variant| {
-				div()
-					.child(variant.glyph)
-					.text_size(gpui::px(24.))
-					.cursor_pointer()
-					.id("hi")
-					.hover(|s| s.bg(hsla(0., 0., 0., 0.1)))
-					.on_click(move |_, _, cx: &mut App| {
-						insert_emoji(variant.glyph);
-						// cx.window().shutdown();
-						cx.shutdown();
-					})
-			}))
-			.into_any_element()
-	} else {
-		div().into_any_element()
-	}
+	let variants = emoji.skin_tones.unwrap();
+
+	h_flex()
+		.gap_2()
+		.p_0()
+		.bg(blue())
+		.children(variants.into_iter().map(|variant| {
+			div()
+				.child(variant.glyph)
+				.text_size(gpui::px(24.))
+				.cursor_pointer()
+				.id("hi")
+				.hover(|s| s.bg(hsla(0., 0., 0., 0.1)))
+				.on_click(move |_, _, cx: &mut App| {
+					insert_emoji(variant.glyph);
+					// cx.window().shutdown();
+					cx.shutdown();
+				})
+		}))
+		.into_any_element()
 }
